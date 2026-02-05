7f08a2b0d6e40505ee486826c9ca23172122d510
hughboi <hughboi-mac@hughboi.cc>
2026-02-05 13:23:14 -0700

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,16 +11,17 @@ variable "proxmox_api_url" {
   type      = string
 }
 
+variable "proxmox_all" {
+  type      = list(string)
+  description = "All proxmox nodes"
+}
+
 variable "proxmox_master" {
   type      = string
+  description = "pve-srv-1"
 }
 
 variable "proxmox_nodes_k3s" { 
   type      = list(string)  # needs to be list(string) if defining multiple nodes
   description = "pve-srv-2, pve-srv-3, pve-srv-4"
-}
-
-variable "proxmox_all" {
-  type      = list(string)
-  description = "All proxmox nodes"
 }
